668c7392507c32b5408743505a66fcfabd1c0cd6
Golias <joao.victor@mobile2you.com.br>
2019-06-30 13:34:28 -0300

    added implicit_variables module

---
+++ src/implicit_variables.R
@@ -0,0 +1,14 @@
+implicit_int <- 10L
+class(implicit_int)
+
+implicit_float <- 10
+class(implicit_float)
+
+implicit_bool <- T
+print(implicit_bool)
+class(implicit_bool)
+implicit_bool = !implicit_bool
+print(implicit_bool)
+
+
+
